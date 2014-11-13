a42d6f4778d610812870ac2f5c34dae51f39fc8e
call8880 <barkaimenu@gmail.com>
2014-11-13 09:11:54 -0500

    more on shape maker also work on the student classes

--- src/Objects/MakeStudents.java
+++ src/Objects/MakeStudents.java
@@ -1,32 +1,32 @@
-package Objects;
-
-public class MakeStudents {
-
-	public static void main(String[] args) {
-		
-		Student s = new Student();
-		s.setName("Bill");
-		s.setScore(1, 71);
-		s.setScore(2, 59);
-		s.setScore(3, 82);
-		System.out.println("The average for " + s.getName() + " is " + s.getAverage());
-		System.out.println("The top score for " + s.getName() + " is " + s.getHighScore());
-		System.out.println(s);//automatically runs the toString method
-		System.out.println("\n\n----------------------------\n");
-		Student s2 = new Student("Bob", 54, 76, 83);
-		System.out.println("The average for " + s2.getName() + " is " + s2.getAverage());
-		System.out.println("The top score for " + s2.getName() + " is " + s2.getHighScore());
-		System.out.println(s2);//automatically runs the toString method
-		System.out.println("\n\n----------------------------\n");
-		//bob sister Mary Lou is regstring to 
-		Student s3 = new Student(s2);
-		s3.setName("Mary Lou");
-		s3.setScore(1, 95);
-		System.out.println("The average for " + s3.getName() + " is " + s3.getAverage());
-		System.out.println("The top score for " + s3.getName() + " is " + s3.getHighScore());
-		System.out.println(s3);
-		System.out.println("s2 has the name " + s2.getName());
-		System.out.println("s2's firt mark is " + s2.getScore(1));
-		
-	} 
-}
+//package Objects;
+//
+//public class MakeStudents {
+//
+//	public static void main(String[] args) {
+//
+//		Student s = new Student();
+//		s.setName("Bill");
+//		s.setScore(1, 71);
+//		s.setScore(2, 59);
+//		s.setScore(3, 82);
+//		System.out.println("The average for " + s.getName() + " is " + s.getAverage());
+//		System.out.println("The top score for " + s.getName() + " is " + s.getHighScore());
+//		System.out.println(s);//automatically runs the toString method
+//		System.out.println("\n\n----------------------------\n");
+//		Student s2 = new Student("Bob", 54, 76, 83);
+//		System.out.println("The average for " + s2.getName() + " is " + s2.getAverage());
+//		System.out.println("The top score for " + s2.getName() + " is " + s2.getHighScore());
+//		System.out.println(s2);//automatically runs the toString method
+//		System.out.println("\n\n----------------------------\n");
+//		//bob sister Mary Lou is regstring to 
+//		Student s3 = new Student(s2);
+//		s3.setName("Mary Lou");
+//		s3.setScore(1, 95);
+//		System.out.println("The average for " + s3.getName() + " is " + s3.getAverage());
+//		System.out.println("The top score for " + s3.getName() + " is " + s3.getHighScore());
+//		System.out.println(s3);
+//		System.out.println("s2 has the name " + s2.getName());
+//		System.out.println("s2's firt mark is " + s2.getScore(1));
+//
+//	} 
+//}
